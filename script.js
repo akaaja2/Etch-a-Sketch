c193b576cd7e907739c7388f7e012b504e281200
Alex <alexj.ash@gmail.com>
2022-08-17 17:09:07 +0100

    BUG add change grid size needs to delete old
    Possible causes; Is not rerunning function but part of script
    push to top of page
    or replace js injection

--- script.js
+++ script.js
@@ -7,13 +7,10 @@
 
 
 //set default size
-var gridSize = 16;
+let gridSize = 16;
 
 //on input click
-function promptGrid () {
-var gridSize = prompt('Enter grid size x by x:');
-console.log(gridSize);
-}
+
 // alternate;
 //document.querySelector('#gridSize').addEventListener('click', promptGrid);
 
@@ -21,6 +18,8 @@ console.log(gridSize);
 let columns = gridSize;
 let rows = gridSize;
 
+console.log(gridSize);
+    
 var grid = document.createElement('div');
 
 grid.className = 'grid';
@@ -40,7 +39,9 @@ for (var i = 0; i < columns; ++i) {
 }
 document.body.appendChild(grid);
 
-// bypass hover function
+
+
+// bypass hover function use mouseover
 grid.addEventListener('mouseover', function (e) {
     //add active class
     if (e.target.matches('.box')) {
@@ -54,3 +55,51 @@ grid.addEventListener('mouseover', function (e) {
       e.target.classList.remove('active');
     }
   });
+  
+  function promptGrid () {
+var gridSized = window.prompt('Enter grid size x by x:');
+
+gridSize = parseInt(gridSized);
+console.log(gridSize);
+
+
+let columns = gridSize;
+let rows = gridSize;
+
+console.log(gridSize);
+    
+var grid = document.createElement('div');
+
+grid.className = 'grid';
+
+for (var i = 0; i < columns; ++i) {
+    var column = document.createElement('div'); // create column
+    column.className = 'column';
+    for (var j = 0; j < rows; ++j) {
+        var row = document.createElement('div'); // create row
+        row.className = 'row';
+        row.classList.add('box'); //add box class
+
+        column.appendChild(row); // append row in column
+    }
+    grid.appendChild(column); // append column inside grid
+    
+}
+document.body.appendChild(grid);
+// bypass hover function use mouseover
+grid.addEventListener('mouseover', function (e) {
+    //add active class
+    if (e.target.matches('.box')) {
+      e.target.classList.add('active');
+    }
+  });
+// add delete function by clicking
+  grid.addEventListener('click', function (e) {
+    //remove active class
+    if (e.target.matches('.box')) {
+      e.target.classList.remove('active');
+    }
+  });
+
+}
+
